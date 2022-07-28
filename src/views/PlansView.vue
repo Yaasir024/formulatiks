44020bb83f672e8c6e808201a25c97e09ea05bfb
Yaasir024 <yope4ever@gmail.com>
2022-07-28 11:12:40 +0100

    /plans header

--- src/views/PlansView.vue
+++ src/views/PlansView.vue
@@ -1,3 +1,28 @@
 <template>
-    Plans
+  <main class="plans-section">
+    <header>
+      <h1>Here are our plans</h1>
+      <p>Free plan Gives You all you need, Switch to Premium for more</p>
+    </header>
+  </main>
 </template>
+
+<style scoped>
+.plans-section {
+  max-width: 750px;
+  margin: 0 auto;
+  padding: 40px 32px;
+}
+header {
+  margin-bottom: 42px;
+  text-align: center;
+}
+header h1 {
+  font-size: 32px;
+  font-weight: 700;
+  margin-bottom: 14px;
+}
+header p {
+  font-size: 18px;
+}
+</style>
